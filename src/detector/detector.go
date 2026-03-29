76437fdc23bb2ab6a55fd8328f779b5225b6e517
BigBiba <nikitafedorov.or@gmail.com>
2026-03-29 22:42:59 +0300

    detector

---
+++ src/detector/detector.go
@@ -0,0 +1,54 @@
+package detector
+
+import (
+	"analizier/src/packet"
+	"fmt"
+)
+
+type AnomalyType int
+
+const (
+	AnomalyNone AnomalyType = iota
+	AnomalyDoS
+	AnomalyOverload
+	AnomalyScanning
+	AnomalyWorm
+	AnomalyP2MP
+	AnomalyFlowSwitching
+	AnomalyVirus
+)
+
+func (a AnomalyType) String() string {
+	switch a {
+	case AnomalyNone:
+		return "None"
+	case AnomalyDoS:
+		return "DoS/DDoS Attack"
+	case AnomalyOverload:
+		return "Network Overload"
+	case AnomalyScanning:
+		return "Network/Port Scanning"
+	case AnomalyWorm:
+		return "Worm Activity"
+	case AnomalyP2MP:
+		return "Point-to-Multipoint"
+	case AnomalyFlowSwitching:
+		return "Flow Switching"
+	case AnomalyVirus:
+		return "Confirmed Virus Activity"
+	default:
+		return fmt.Sprintf("Unknown Anomaly (%d)", a)
+	}
+}
+
+type DetectionResult struct {
+	IsAnomaly  bool
+	Confidence float64
+	Type       AnomalyType
+}
+
+type Detector interface {
+	// Возвращает имя детектора. Может пригодиться для отладочной информации
+	Name() string
+	Analyze(stats packet.FlowStats) DetectionResult
+}
